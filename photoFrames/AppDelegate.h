07fe2856511055ceb87b4b378b1a64631b5b9668
ValeriySoglaev <ValeriySoglaev@z-mac.local>
2013-12-16 22:36:57 +0200

    Initial Commit

---
+++ photoFrames/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  photoFrames
+//
+//  Created by ValeriySoglaev on 16.12.13.
+//  Copyright (c) 2013 Valeriy Soglaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ photoFrames/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  photoFrames
+//
+//  Created by ValeriySoglaev on 16.12.13.
+//  Copyright (c) 2013 Valeriy Soglaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
